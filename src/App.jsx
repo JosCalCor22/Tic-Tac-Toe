5e54b5ebe12521c382255e5c0d9e37cc88abc7b7
Joseth Calderon <stevencorredor04@gmail.com>
2024-02-28 22:55:52 -0500

    Adding a table section to give a score to the users

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import { GameBoard } from './components/gameBoard';
 import { TableResultGame } from './components/tableResultsGame';
 import { COMBOS_WINNER, Turns, status } from './components/variables';
 import './styles/App.css';
+import './styles/FrontPageApp.css';
 
 function App() {
+  /* States for the componets */
+  const [frontPageGame, setFrontPageGame] = useState(false);
+  const [pageBoardGame, setPageBoardGame] = useState(true);
+  const [resultGame, setResultGame] = useState(false);
+
+  /* States for the game */
   const [board, setBoard] = useState(Array(9).fill(null));
   const [winner, setWinner] = useState(status.playing);
   const [turn, setTurn] = useState(Turns.X);
+  
+  /* States for the table */
+  const [victoryX, setVictoryX] = useState(0);
+  const [defeatX, setDefeatX] = useState(0);
+  const [defeatO, setDefeatO] = useState(0);
+  const [victoryO, setVictoryO] = useState(0);
 
-  const className2 = winner !== status.playing ? 'board__decision' : 'inactive';
+  const className2 = resultGame ? 'board__decision' : 'inactive';
   
   const newBoard = [...board];
 
@@ -25,6 +38,17 @@ function App() {
       ){
         setTurn(boardCheck[a]);
         setWinner(status.winner);
+        setResultGame(true);
+        setPageBoardGame(false);
+
+        if(boardCheck[a] === Turns.X){
+          setVictoryX(victoryX + 1);
+          setDefeatO(defeatO + 1);
+        } else{
+          setVictoryO(victoryO + 1);
+          setDefeatX(defeatX + 1);
+        }
+
       } else{
         if(isDraw){
           setWinner(status.draw);
@@ -57,19 +81,44 @@ function App() {
   };
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null));
+    setDefeatO(0);
+    setDefeatX(0);
+    setVictoryX(0);
+    setVictoryO(0);
     setTurn(Turns.X);
+    setResultGame(false);
+    setPageBoardGame(true);
     setWinner(status.playing);
+    setBoard(Array(9).fill(null));
+  }
+
+  const revangeGame = () => {
+    setPageBoardGame(true);
+    setWinner(status.playing);
+    setBoard(Array(9).fill(null));
   }
 
   return (
     <>
       <main className='board'>
-        <GameBoard board={board} updateBoard={updateBoard} selectTurn={turn} />
-
-        <section className={className2}>
-          <TableResultGame playerWinner={winner} selectTurn={turn} resetGame={resetGame} />
-        </section>
+        { pageBoardGame ? (
+            <GameBoard board={board} updateBoard={updateBoard} selectTurn={turn} />
+        ) : resultGame ? (
+          <section className={className2}>
+            <TableResultGame 
+            selectTurn={turn} 
+            defeatX={defeatX}
+            defeatO={defeatO}
+            victoryX={victoryX}
+            victoryO={victoryO}
+            playerWinner={winner} 
+            resetGame={resetGame}
+            revangeGame={revangeGame} />
+          </section>
+        ) : (
+          null
+        )
+        }
       </main>
     </>
   )
--- src/components/tableResultsGame.jsx
+++ src/components/tableResultsGame.jsx
@@ -1,18 +1,48 @@
 import { Turns, status } from './variables';
 
-function TableResultGame ({playerWinner, selectTurn, resetGame}){
+function TableResultGame ({ playerWinner, selectTurn, resetGame, defeatX, defeatO, victoryX, victoryO, revangeGame }) {
   return(
     <>
       {
         playerWinner !== status.playing && (
-          <div className="board__text">
-            <h2>
-              {
-                playerWinner === status.winner ? `${selectTurn === Turns.X ? 'X' : 'O'} es el ganador`
-                : 'Hubo un empate'
-              }
-            </h2>
-            <button onClick={resetGame}>Reiniciar Juego</button>
+          <div className="board__result">
+            <div className="board__result--title">
+              <h2>
+                {
+                  playerWinner === status.winner ? `${selectTurn === Turns.X ? 'X' : 'O'} es el ganador`
+                  : 'Hubo un empate'
+                }
+              </h2>
+              <p>¡Gracias por jugar! <br />¿Revancha o miedo?😎</p>
+              <span>Marcador:</span>
+            </div>
+            <div className="board__result--table">
+              <table className='result__table'>
+                <thead>
+                  <tr>
+                    <td>Jugador</td>
+                    <td>Victorias</td>
+                    <td>Derrotas</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className='result__table--player1'>
+                    <td>Player 1</td>
+                    <td>{victoryX}</td>
+                    <td>{defeatX}</td>
+                  </tr>
+                  <tr className='result__table--player2'>
+                    <td>Player 2</td>
+                    <td>{victoryO}</td>
+                    <td>{defeatO}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="board__result--buttons">
+              <button onClick={revangeGame}>¡Revancha!</button>
+              <button onClick={resetGame}>Juego Nuevo</button>
+            </div>
           </div>
         )
       }
